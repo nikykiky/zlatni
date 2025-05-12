849b7bf64a9f2aecd97f835571bc1c7d247e0db2
Mario <marioradman07@gmail.com>
2025-05-12 15:50:48 +0200

    "promjena"

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Stručna služba</title>
+
     <meta http-equiv="refresh" content="0;url=./dnevnik/dnevnik_rada.php">
     <!-- redirekcija -->
     <link href="admin_css.css" rel="stylesheet" type="text/css" />
